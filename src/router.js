368ecbb7b2f191e96f930a88a70d3597d86f6fb1
Bobae Kang <bkang.kr@gmail.com>
2018-11-16 17:02:41 -0600

    use a module for the router setup

--- src/router.js
+++ src/router.js
@@ -1,44 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+import * as utils from './utils'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
-        {
-            path: '/',
-            component: Home
-        },
-        {
-            path: '/data',
-            component: () => import('./views/Data.vue')
-        },
-        {
-            path: '/research',
-            component: () => import('./views/Research.vue')
-        },
-        {
-            path: '/apps',
-            component: () => import('./views/Apps.vue')
-        },
-        {
-            path: '/resources',
-            component: () => import('./views/Resources/Index.vue'),
-            children: [
-                {
-                    path: 'user',
-                    component: () => import('./views/Resources/User.vue')
-                },
-                {
-                    path: 'developer',
-                    component: () => import('./views/Resources/Developer.vue')
-                }
+        utils.makeRoute('home'),
+        utils.makeRoute('dataset'),
+        utils.makeRoute('research'),
+        utils.makeRoute('apps'),
+        utils.makeRoute(
+            'resources',
+            './views/Resources',
+            [
+                utils.makeRouteChild('user', './views/Resources'),
+                utils.makeRouteChild('developer', './views/Resources')
             ]
-        },
-        {
-            path: '/about',
-            component: () => import('./views/About.vue')
-        }
+        ),
+        utils.makeRoute('about')
     ]
 })
+
+
+
---
+++ src/utils.js
@@ -0,0 +1,38 @@
+const _getPath = (name, child) => {
+    if (name == 'home') {
+        return '/';
+    } else {
+        return child ? name : `/${name}`;
+    }
+}
+
+const _getCompPath = (name, dirpath) => {
+    return `${dirpath}/${name.charAt(0).toUpperCase() + name.slice(1)}.vue`
+}
+
+// Generate a route object for 'router.js'
+export function makeRoute(name, dirpath = './views', children = null) {  
+    const path = _getPath(name, false);
+    const compPath = _getCompPath(name, dirpath);
+    
+    let route = {
+        path,
+        component: () => import(`${compPath}`)
+    }
+
+    if (children != null) {
+        route.children = children;
+    }
+    
+    return route;
+}
+
+export function makeRouteChild(name, dirpath = './views') {
+    const path = _getPath(name, true);
+    const compPath = _getCompPath(name, dirpath);
+    
+    return {
+        path,
+        component: () => import(`${compPath}`)
+    };
+}
