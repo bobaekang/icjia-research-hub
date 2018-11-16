8dd54d1d64902a05b8e0ee07ad83be89ba1dfbcb
Bobae Kang <bkang.kr@gmail.com>
2018-11-16 17:06:23 -0600

    add icjia logo

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -2,8 +2,18 @@
     <div>
         <v-toolbar
             id="toolbar"
+            :height=hpixel
             fixed
             scroll-off-screen>
+            
+            <a href="http://www.icjia.state.il.us" target="_blank">
+                <img
+                    src="http://www.icjia.state.il.us/_themes/icjia/img/logo-icjia-small-blue-3.png"
+                    alt="ICJIA logo"
+                    :height=logoHpixel
+                    >
+            </a>
+
             <router-link to="/">
                 <v-toolbar-title>
                     ICJIA DATA PORTAL
@@ -13,49 +23,51 @@
             <v-spacer></v-spacer>
 
             <v-toolbar-items>
-                <v-btn to="/data" flat>
-                    DATA
-                </v-btn>
-                
-                <v-btn to="/research" flat>
-                    RESEARCH
-                </v-btn>
-                
-                <v-btn to="/apps" flat>
-                    APPS
-                </v-btn>
-                
+                <v-btn to='/dataset' flat>dataset</v-btn>
+                <v-btn to="/research" flat>research</v-btn>
+                <v-btn to="/apps" flat>apps</v-btn>
                 <v-menu offset-y>
-                    <v-btn slot="activator" flat>
-                        RESOURCES
-                    </v-btn>
+                    <v-btn slot="activator" flat>resources</v-btn>
                     <v-list>
                         <v-list-tile
-                            to="/Resources/User"
+                            to="/resources/user"
                             ripple>
-                            <span class="body-1">USER</span>
+                            <span class="body-1">
+                                {{'user'.toUpperCase()}}
+                            </span>
                         </v-list-tile>
                         <v-list-tile
-                            to="/Resources/Developer"
+                            to="/resources/developer"
                             ripple>
-                            <span class="body-1">DEVELOPER</span>
+                            <span class="body-1">
+                                {{'developer'.toUpperCase()}}
+                            </span>
                         </v-list-tile>
                     </v-list>
                 </v-menu>
-                
-                <v-btn to="/about" flat>
-                    ABOUT
-                </v-btn>
+                <v-btn to="/about" flat>about</v-btn>
             </v-toolbar-items>
         </v-toolbar>
-        <div style="height:75px">
-        </div>
+
+        <div :style="{ height: hpixel }"></div>
     </div>
 </template>
 
 <script>
 export default {
-
+    data () {
+        return {
+            height: 75
+        }
+    },
+    computed: {
+        hpixel () {
+            return `${this.height}px`
+        },
+        logoHpixel () {
+            return `${this.height * 0.85}px`
+        }
+    }
 }
 </script>
 
@@ -63,6 +75,10 @@ export default {
 a {
     text-decoration:none;
     color:rgba(0,0,0,.87);
+}
+
+img {
+    margin-right: 10px;
 }
 
 #toolbar {
