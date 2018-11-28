ecfba00e5cbe0dfe8626a8b572ad9bf06e193482
Bobae Kang <bkang.kr@gmail.com>
2018-11-28 16:54:09 -0600

    switch to cards to show articles

--- src/components/ArticleSnippet.vue
+++ src/components/ArticleSnippet.vue
@@ -1,20 +1,53 @@
 <template>
-    <v-container>
-        <v-container
-            class="article-snippet mb-3"
+    <v-layout row wrap>
+        <v-flex
+            xs10
+            offset-xs1
+            class="mb-3"
             v-for="(item,i) in items"
             :key="i"
             >
-            <h2>{{ item.title }}</h2>
-            <h4>{{ item.date }}</h4>
-            <v-img
-                :src="`http://www.icjia.state.il.us/${item.splash}`"
-                max-width="40%"
-                />
-            <v-container>{{ item.teaser }}</v-container>
-            <h4>by {{ item.authors.join(', ') }}</h4>
-        </v-container>
-    </v-container>
+            <v-card class="article-card">
+                <v-layout row>
+                    <v-img
+                        :src="`http://www.icjia.state.il.us/${item.splash}`"
+                        />
+                    <v-layout
+                        column
+                        justify-space-between
+                        class="article-body"
+                        >
+                        <div>
+                            <h3>{{ item.title }}</h3>
+                            <h5>by {{ item.authors.join(', ') }}</h5>
+                            <h5>{{ item.date }}</h5>
+                        </div>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn flat @click="item.show = !item.show">
+                                summary
+                                <v-icon>{{ item.show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                            </v-btn>
+                            <v-btn
+                                :href="`http://www.icjia.state.il.us/articles/${item.filename}`"
+                                target="_blank"
+                                flat>
+                                full
+                            </v-btn>
+                        </v-card-actions>
+
+                        <v-slide-y-transition>
+                            <v-card-text v-show="item.show">
+                                {{ item.teaser }}
+                            </v-card-text>
+                        </v-slide-y-transition>
+                    </v-layout>
+
+                </v-layout>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
@@ -31,7 +64,26 @@ export default {
 
 <style scoped>
 .article-snippet {
-    border: 1px solid #ccc;
+    /* border: 1px solid #ccc; */
+}
+
+.article-card {
+    overflow: hidden;
+	width: 100%;
+
+	border-radius: 0.1em;
+}
+
+.article-card img {
+	width: 240px;
+	height: 200px;
+
+	object-fit: cover;
+}
+
+.article-body {
+    padding: 0.8em 1.3em;
+    max-width: 540px;
 }
 
 iframe {
