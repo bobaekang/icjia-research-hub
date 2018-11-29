ff1dcc0fb8f9f512684cb3350e77926c80969481
Bobae Kang <bkang.kr@gmail.com>
2018-11-29 14:27:20 -0600

    use json for app data

--- src/components/CardApp.vue
+++ src/components/CardApp.vue
@@ -1,35 +1,38 @@
 <template>
     <v-container fluid>
         <v-layout row wrap>
-            <v-flex sm6 xs12
-                v-for="(item,i) in items"
+            <v-flex
+                md4
+                sm6
+                xs12
+                v-for="(item,i) in editedItems"
                 :key="i"
                 >
                 
                 <v-card class="ma-3">
                     <v-img
-                        :src="item.img"
+                        :src="item.imgUrl"
                         height="200px">
                     </v-img>
 
                     <v-card-title primary-title>
                         <div>
                             <div class="headline">{{ item.title }}</div>
-                            <span class="grey--text">{{ item.descShort }}</span>
+                            <span class="grey--text">{{ item.subtitle }}</span>
                         </div>
                     </v-card-title>
 
                     <v-card-actions>
                         <v-btn :href=item.url flat>Launch</v-btn>
                         <v-spacer></v-spacer>
-                        <v-btn icon @click="item.show = !item.show">
-                            <v-icon>{{ item.show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                        <v-btn icon @click="item.showDesc = !item.showDesc">
+                            <v-icon>{{ item.showDesc ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
                         </v-btn>
                     </v-card-actions>
 
                     <v-slide-y-transition>
-                        <v-card-text v-show="item.show">
-                            {{ item.descLong }}
+                        <v-card-text v-if="item.showDesc">
+                            {{ item.desc }}
                         </v-card-text>
                     </v-slide-y-transition>
                 </v-card>
@@ -40,41 +43,19 @@
 </template>
 
 <script>
+import appInfo from '@/assets/appInfo.json';
+
 export default {
     data () {
         return {
-            items: [
-                {
-                    img: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-                    title: "Application 1",
-                    descShort: "This is an awesome application",
-                    url: "http://www.icjia.state.il.us/",
-                    show: false,
-                    descLong: "More detailed description from the contributor."  
-                },
-                {
-                    img: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-                    title: "Application 2",
-                    descShort: "This is another application",
-                    url: "http://www.icjia.state.il.us/",
-                    show: false,
-                    descLong: "More detailed description from the contributor."  
-                },
-                {
-                    img: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-                    title: "Application 3",
-                    descShort: "Yet another application",
-                    url: "http://www.icjia.state.il.us/",
-                    show: false,
-                    descLong: "More detailed description from the contributor."  
-                }
-            ]
+            items: appInfo
         }
     },
-    methods: {
-        toggleShow: function(item) {
-            item.show = !item.show;
-            return item.show;
+    computed: {
+        editedItems () {
+            return this.items.map(item => {
+                return item;
+            });
         }
     }
 }
