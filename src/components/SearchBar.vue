a3653e2949ae61fcefb83cf1060016e05a36bbd0
Bobae Kang <bkang.kr@gmail.com>
2018-11-29 14:11:24 -0600

    create search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-text-field
+        @keyup="onChange($event)"
+        :value="searchInput"
+        :label="searchLabel"
+        append-icon="search"
+        solo
+        />
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+        search: String
+    },
+    data () {
+        return {
+            searchInput: this.search,
+            searchLabel: this.label
+        }
+    },
+    methods: {
+        onChange (e) {
+            this.$emit('update:search', e.target.value);
+        }
+    }
+}
+</script>
