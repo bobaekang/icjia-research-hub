722db7e66836dcf9de7bd9e39a08413cc92d47c8
Bobae Kang <bkang.kr@gmail.com>
2018-11-29 13:54:15 -0600

    add more info & apply img lazy load & use computed for items

--- src/components/ArticleSnippet.vue
+++ src/components/ArticleSnippet.vue
@@ -1,53 +1,82 @@
 <template>
-    <v-layout row wrap>
-        <v-flex
-            xs10
-            offset-xs1
-            class="mb-3"
-            v-for="(item,i) in items"
-            :key="i"
-            >
-            <v-card class="article-card">
-                <v-layout row>
-                    <v-img
-                        :src="`http://www.icjia.state.il.us/${item.splash}`"
-                        />
-                    <v-layout
-                        column
-                        justify-space-between
-                        class="article-body"
-                        >
-                        <div>
-                            <h3>{{ item.title }}</h3>
-                            <h5>by {{ item.authors.join(', ') }}</h5>
-                            <h5>{{ item.date }}</h5>
-                        </div>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex
+                xs12 sm10
+                offset-sm1
+                class="mb-3"
+                v-for="(item, i) in editedItems"
+                :key="i"
+                >
+                <v-card class="article-card">
+                    <v-layout row>
+                        <v-img
+                            class="hidden-sm-and-down"
+                            :src="item.imgUrl"
+                            lazy-src="https://via.placeholder.com/1/DDDDDD"
+                            >
+                            <v-layout
+                                slot="placeholder"
+                                fill-height
+                                align-center
+                                justify-center
+                                ma-0
+                                >
+                                <v-progress-circular
+                                    indeterminate color="grey lighten-3"
+                                    />
+                            </v-layout>
+                        </v-img>
+                        <v-layout
+                            column
+                            justify-space-between
+                            class="article-body"
+                            >
+                            <div>
+                                <h3 style="padding-bottom: 10px;">
+                                    {{ item.title }}
+                                </h3>
+                                <h5 style="padding-bottom: 10px;">
+                                    {{ item.date }} |
+                                    {{ item.pubtype }} |
+                                    {{ item.area }}
+                                </h5>
+                                <h5>
+                                    By
+                                    {{ item.authors }}
+                                </h5>
+                            </div>
 
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn flat @click="item.show = !item.show">
-                                summary
-                                <v-icon>{{ item.show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-                            </v-btn>
-                            <v-btn
-                                :href="`http://www.icjia.state.il.us/articles/${item.filename}`"
-                                target="_blank"
-                                flat>
-                                full
-                            </v-btn>
-                        </v-card-actions>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                    v-if="item.teaser"
+                                    @click="item.showTeaser = !item.showTeaser"
+                                    flat
+                                    >
+                                    summary
+                                    <v-icon>{{ item.showTeaser ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                                </v-btn>
+                                <v-btn
+                                    :href="item.url"
+                                    target="_blank"
+                                    flat>
+                                    full
+                                    <v-icon>open_in_new</v-icon>
+                                </v-btn>
+                            </v-card-actions>
 
-                        <v-slide-y-transition>
-                            <v-card-text v-show="item.show">
-                                {{ item.teaser }}
-                            </v-card-text>
-                        </v-slide-y-transition>
+                            <v-slide-y-transition>
+                                <v-card-text v-if="item.showTeaser">
+                                    {{ item.teaser }}
+                                </v-card-text>
+                            </v-slide-y-transition>
+                        </v-layout>
                     </v-layout>
-
-                </v-layout>
-            </v-card>
-        </v-flex>
-    </v-layout>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
@@ -56,42 +85,41 @@ import articleInfo from '@/assets/articleInfo.json';
 export default {
     data () {
         return {
-            items: articleInfo
+            items: articleInfo,
         }
-    }
+    },
+    computed: {
+        editedItems () {
+            const joinIfArray = (x, name = false) => {
+                if (Array.isArray(x)) {
+                    if (name && x.length > 1) {
+                        let i = x.length - 1
+                        return x.slice(0, i).join(', ') + ' and ' + x[i];
+                    } else {
+                        return x.join(', ')
+                    }
+                } else {
+                    return x
+                }
+            }
+
+            return this.items.map(item => {
+                item.area = joinIfArray(item.area);
+                item.authors = joinIfArray(item.authors, true);
+                item.pubtype = joinIfArray(item.pubtype);
+                item.url = `http://www.icjia.state.il.us/articles/${item.filename}`;
+                item.imgUrl = `http://www.icjia.state.il.us/${item.splash}`;
+
+                return item;
+            });
+        },
+    },
 }
 </script>
 
 <style scoped>
-.article-snippet {
-    /* border: 1px solid #ccc; */
-}
-
-.article-card {
-    overflow: hidden;
-	width: 100%;
-
-	border-radius: 0.1em;
-}
-
-.article-card img {
-	width: 240px;
-	height: 200px;
-
-	object-fit: cover;
-}
-
 .article-body {
-    padding: 0.8em 1.3em;
-    max-width: 540px;
-}
-
-iframe {
-    width: 100%;
-    height: 500px;
-}
-
-iframe > #document {
-    display: none;
+    padding: 1em 0.5em 0 1.2em;
+    width: 100px;
 }
 </style>
