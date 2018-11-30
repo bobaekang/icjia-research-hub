bb56380c431324b097fa17b9009876edb6a9e1f3
Bobae Kang <bkang.kr@gmail.com>
2018-11-30 16:43:02 -0600

    add search bar and more

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,16 +1,48 @@
 <template>
     <v-container>
-        <h2>{{ title }}</h2>
-        <h3>With tag-specific buttons</h3>
+        <v-layout
+            justify-center
+            row
+            wrap
+            >
+            <v-flex
+                xs12
+                class="text-xs-center"
+                >
+                <h3>{{ titleUpper }}</h3>
+                <v-icon x-large>arrow_drop_down</v-icon>
+            </v-flex>
+            
+            <v-flex
+                xs12
+                sm9
+                >
+                <app-search-bar
+                    label="Search"
+                    :search.sync="search"
+                    />
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 
 <script>
+import AppSearchBar from './SearchBar';
+
 export default {
     data () {
         return {
-            title: "Search for the data!"
+            title: 'get started and explore this data portal',
+            search: ''
         }
+    },
+    computed: {
+        titleUpper () {
+            return this.title.toUpperCase();
+        }
+    },
+    components: {
+        AppSearchBar
     }
 }
 </script>
