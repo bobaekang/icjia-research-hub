fa70da9a5a4b2fead923092ed8c21251c44fc83a
Bobae Kang <bkang.kr@gmail.com>
2018-12-12 16:36:41 -0600

    add links and use year dynamically

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,9 +7,19 @@
             row
             wrap
             >
-            <v-flex
-                text-xs-center>
-                &copy; 2019 Illinois Criminal Justice Information Authority
+            <v-flex text-xs-center>
+                <span>
+                    {{ copy + ' ' + year }}
+                    <a :href="agency.url">{{ agency.name }}</a>
+                </span>
+                |
+                <span>
+                    <a :href="github.url">
+                        {{ version }}
+                        <v-icon>{{ github.icon }}</v-icon>
+                    </a>
+                </span>
+
             </v-flex>
         </v-layout>
     </v-footer>
@@ -17,7 +27,21 @@
 
 <script>
 export default {
-
+    data () {
+        return {
+            copy: '©',
+            year: new Date().getFullYear(),
+            agency: {
+                name: 'Illinois Criminal Justice Information Authority',
+                url: 'http://www.icjia.state.il.us/'
+            },
+            version: '0.1.0',
+            github: {
+                icon: 'fa fa-github',
+                url: 'https://github.com/bobaekang/icjia-data-portal-proto'
+            },
+        }        
+    }
 }
 </script>
 
