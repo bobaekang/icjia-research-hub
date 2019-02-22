d92dcd6201af4549877ccdc64c776133aa0b66dc
Bobae Kang <bkang.kr@gmail.com>
2019-02-22 17:02:35 -0600

    update to reflect new schema and styling

--- src/components/ArticleItemView.vue
+++ src/components/ArticleItemView.vue
@@ -2,7 +2,7 @@
     <div>
         <v-img
             height="500px"
-            :src="`${base_url}/${article.splash.url}`"
+            :src="article.splash"
             >
         </v-img>
 
@@ -61,14 +61,14 @@
                             class="article-authors"
                             >
                             <span v-if="isBeforeLastAuthor(article.authors.length, i)">
-                                and&nbsp;
+                                &nbsp;and&nbsp;
                             </span>
                             
                             <router-link :to="`/author/${author.slug}`">
                                 {{ author.title }}
                             </router-link>
                             
-                            <span v-if="i + 1 < article.authors.length">
+                            <span v-if="i + 2 < article.authors.length">
                                 ,&nbsp;
                             </span>
                         </span>
@@ -87,13 +87,10 @@
                         v-html="compliedBody"
                         />
 
-                    <div style="height: 500px"></div>
+                    <div style="height: 100px"></div>
                 </v-flex>
-
             </v-layout>        
         </v-container>
-
-        
     </div>
 </template>
 
@@ -119,20 +116,6 @@ export default {
             return this.item;
         },
         compliedBody () {
-            // let test = "# Heading (#)\n"
-            // test += "Some contents[^1]\n"
-            // test += "## Sub-heading (##)\n"
-            // test += "Some contents[^2]\n"
-            // test += "### Sub-sub-heading (###)\n"
-            // test += "Some contents\n"
-            // test += "#### Sub-sub-sub-heading (####)\n"
-            // test += "Some contents\n"
-            // test += "##### Sub-sub-sub-sub-heading (#####)\n"
-            // test += "Some contents\n"
-            // test += "[^1]: First footnote\n"
-            // test += "[^2]: Second footnote\n"
-            
-            // return md.render(test);
             return md.render(this.item.markdown);
         }
     },
@@ -149,77 +132,6 @@ export default {
     font-size: 16px;
     text-transform: uppercase;
     font-family: 'Oswald', sans-serif;
-}
-
-.article-body >>> h1,
-.article-body >>> h2,
-.article-body >>> h3,
-.article-body >>> h4,
-.article-body >>> h5,
-.article-body >>> h6 {
-    font-family: 'Oswald',sans-serif;
-    line-height: 1.3;
-}
-
-.article-body >>> h1,
-.article-body >>> h2,
-.article-body >>> h3 {
-    margin-top: 22px;
-    margin-bottom: 11px;
-    font-weight: 500;
-}
-
-.article-body >>> h4,
-.article-body >>> h5,
-.article-body >>> h6 {
-    text-transform: uppercase;
-    color: black;
-    letter-spacing: .04em;
-    font-weight: 700;
-    line-height: 26px;
-}
-
-.article-body >>> h1 {
-    font-size: 48px;
-    font-weight: 700;
-}
-
-.article-body >>> h2 {
-    font-size: 34px;
-}
-
-.article-body >>> h3 {
-    font-size: 24px;
-}
-
-.article-body >>> h4 {
-    font-size: 17px;
-}
-
-.article-body >>> h5 {
-    font-size: 14px;
-}
-
-.article-body >>> p {
-    font-size: 22px;
-    text-indent: 1.5em;
-}
-
-.article-body >>> hr {
-    margin-top: 22px;
-    margin-bottom: 22px;
-    border: 0;
-    border-top: 1px solid #eee;
-}
-
-.article-body >>> .footnotes {
-    margin-top: 30px;
-    margin-bottom: 30px;
-}
-
-.article-body >>> .footnotes p {
-    font-size: 16px;
-    text-indent: 0;
 }
 
 .article-date {
@@ -248,6 +160,175 @@ export default {
     font-family: 'Lato', sans-serif;
     font-weight: 300;
     font-size: 20px;
+}
+
+
+/* headers */
+.article-body >>> h1,
+.article-body >>> h2,
+.article-body >>> h3,
+.article-body >>> h4,
+.article-body >>> h5 {
+    font-family: 'Oswald',sans-serif;
+    line-height: 1.3;
+}
+
+.article-body >>> h1,
+.article-body >>> h2,
+.article-body >>> h3 {
+    margin-top: 22px;
+    margin-bottom: 11px;
+    font-weight: 500;
+}
+
+.article-body >>> h4,
+.article-body >>> h5,
+.article-body >>> h6 {
+    text-transform: uppercase;
+    color: black;
+    letter-spacing: .04em;
+    font-weight: 700;
+    margin-bottom: 11px;
+    margin-top: 11px;
+}
+
+.article-body >>> h1 {
+    font-size: 48px;
+    font-weight: 700;
+}
+
+.article-body >>> h2 {
+    font-size: 34px;
+}
+
+.article-body >>> h3 {
+    font-size: 24px;
+}
+
+.article-body >>> h4 {
+    font-size: 17px;
+}
+
+.article-body >>> h5 {
+    font-size: 14px;
+}
+
+.article-body >>> h6 {
+    font-family: 'Lato';
+    font-size: 13px;
+}
+
+.article-body >>> p,
+.article-body >>> ul,
+.article-body >>> ol {
+    font-size: 20px;
+    margin-bottom: 11px;
+}
+
+.article-body >>> ul,
+.article-body >>> ol {
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+}
+
+
+.article-body >>> p {
+    text-indent: 1.5em;
+    
+}
+
+.article-body >>> hr {
+    background: transparent;
+    border: 0;
+    border-bottom: 1px solid #dfe2e5;
+    height: 0;
+    margin: 15px 0;
+    overflow: hidden;
+}
+
+
+/* footnotes */
+.article-body >>> .footnotes {
+    margin-top: 30px;
+    margin-bottom: 30px;
+}
+
+.article-body >>> .footnotes p {
+    font-size: 16px;
+    text-indent: 0;
+}
+
+
+/* figure and table */
+.article-body >>> .article-figure {
+   margin-left:auto; 
+   margin-right:auto;
+   text-align: center;
+   padding: 24px 12px;
+}
+
+.article-body >>> .article-figure h6 {
+
+}
+
+.article-body >>> .article-table {
+    margin-left:auto; 
+    margin-right:auto;
+    text-align: center;
+    padding: 24px 12px; 
+}
+
+.article-body >>> table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    margin-left:auto; 
+    margin-right:auto;
+}
+
+.article-body >>> table th {
+    font-weight: 600;
+}
+
+.article-body >>> table td,
+.article-body >>> table th {
+    border: 1px solid grey;
+    padding: 6px 13px;
+}
+
+.article-body >>> table tr {
+    background-color: #fff;
+    border-top: 1px solid #c6cbd1;
+}
+
+.article-body >>> table tr:nth-child(2n) {
+    background-color: #f6f8fa;
+}
+
+
+/* blockquote */
+.article-body >>> blockquote {
+  border-left: .25em solid #dfe2e5;
+  font-family: 'Lato', sans-serif;
+  color: #6a737d;
+  padding: 0 1em;
+  margin: 2em 0;
+}
+
+.article-body >>> blockquote p,
+.article-body >>> blockquote ol,
+.article-body >>> blockquote ul {
+    font-size: 18px;
+}
+
+.article-body >>> blockquote>:first-child {
+  margin-top: 0;
+}
+
+.article-body >>> blockquote>:last-child {
+  margin-bottom: 0;
 }
 
 </style>
