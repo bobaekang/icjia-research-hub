79c6519f6123dd174a115719cc77e87c092c92ed
Bobae Kang <bkang.kr@gmail.com>
2019-03-14 10:42:36 -0500

    experimenting...

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,25 @@
+// var path = require('path')
+// const PrerenderSPAPlugin = require('prerender-spa-plugin')
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+
 module.exports = {
-  baseUrl:
-    process.env.NODE_ENV === 'production'
-      ? '/icjia-data-portal-proto-dist/'
-      : '/'
+  // baseUrl:
+  //   process.env.NODE_ENV === 'production'
+  //     ? '/icjia-data-portal-proto-dist/'
+  //     : '/',
+  // configureWebpack: config => {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     config.plugins = [
+  //       new PrerenderSPAPlugin({
+  //         staticDir: path.join(__dirname, 'dist'),
+  //         routes: ['/', '/about'],
+  //         renderer: new Renderer({
+  //           renderAfterElementExists: '#app',
+  //           renderAfterTime: 5000,
+  //           headless: true
+  //         })
+  //       })
+  //     ]
+  //   }
+  // }
 }
