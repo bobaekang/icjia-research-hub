626754a294df2764fa0461bc99088b7b0ae1e5a0
Bobae Kang <bkang.kr@gmail.com>
2019-03-29 17:30:37 -0500

    refactor about using slot

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-container pt-0>
+      <v-layout justify-center>
+        <v-flex xs12 sm10 xl8 class="background-white">
+          <v-container>
+            <v-layout justify-center pt-3 pb-5>
+              <h2 class="uppercase light large">- {{ title }} -</h2>
+            </v-layout>
+
+            <v-container py-0>
+              <v-layout row wrap justify-center>
+                <v-flex xs12 sm10 xl8>
+                  <div v-if="subNum" class="pb-3">
+                    <template v-for="sub in subNum">
+                      <h2 :key="sub" class="pb-3 light">
+                        <slot :name="`subtitle${sub}`"></slot>
+                      </h2>
+
+                      <p :key="sub"><slot :name="`subbody${sub}`"></slot></p>
+                    </template>
+                  </div>
+
+                  <slot></slot>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    subNum: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.background-white {
+  background-color: white;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -4,162 +4,131 @@
       <h1>{{ title }}</h1>
     </BaseViewTitle>
 
-    <BaseSection title="This Research Hub">
-      <v-container py-0>
-        <v-layout row wrap justify-center>
-          <v-flex xs12 sm10 xl8>
-            <h3>Open data, open research, open government</h3>
-            <p>Explain the goals and purposes of the Research Hub here.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
-              voluptas laboriosam ab recusandae excepturi eaque dolor id ex
-              reiciendis possimus itaque labore natus quibusdam mollitia
-              perspiciatis facere, dolores quam! Explicabo, magni. Doloremque,
-              corporis ab saepe culpa aliquid dolorem debitis nam!
-            </p>
+    <AboutSection :title="section1.title" :subNum="3">
+      <template v-slot:subtitle1>{{ section1.sub1.title }}</template>
+      <template v-slot:subbody1>
+        <p v-html="section1.sub1.p1"></p>
+        <p v-html="section1.sub1.p2"></p>
+      </template>
 
-            <h3>Open source technology</h3>
-            <p>
-              The Research Hub is developed publicly on
-              <a href="#"> GitHub<v-icon>open_in_new</v-icon> </a>
-              with XXX license.
-            </p>
-            <p>
-              It is also powered by popular open source projects, including:
-            </p>
-            <ul>
-              <li>
-                <a href="https://vuejs.org/">
-                  Vue.js<v-icon>open_in_new</v-icon>
-                </a>
-                and
-                <a href="https://vuetifyjs.com/">
-                  Vuetify<v-icon>open_in_new</v-icon>
-                </a>
-                for user interface;
-              </li>
-              <li>
-                <a href="https://vuepress.vuejs.org/">
-                  VuePress<v-icon>open_in_new</v-icon>
-                </a>
-                for documentation;
-              </li>
-              <li>
-                <a href="https://strapi.io/">
-                  Strapi<v-icon>open_in_new</v-icon>
-                </a>
-                for headless contenet management system;
-              </li>
-              <li>
-                <a href="https://www.docker.com/">
-                  Docker<v-icon>open_in_new</v-icon>
-                </a>
-                for containerization;
-              </li>
-              <li>
-                and many more.
-              </li>
-            </ul>
+      <template v-slot:subtitle2>{{ section1.sub2.title }}</template>
+      <template v-slot:subbody2>
+        <p v-html="section1.sub2.p1"></p>
+        <p v-html="section1.sub2.p2"></p>
+      </template>
 
-            <h3>Asking for your feedback and contribution</h3>
-            <p>Explain how to provide feedback and contribution.</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Obcaecati nemo odio repellendus, quam minus dicta expedita
-              corporis molestiae sequi asperiores!
-            </p>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </BaseSection>
+      <template v-slot:subtitle3>{{ section1.sub3.title }}</template>
+      <template v-slot:subbody3>
+        <p v-html="section1.sub3.p1"></p>
+        <p v-html="section1.sub3.p2"></p>
+      </template>
+    </AboutSection>
 
-    <BaseSection title="Illinois Criminal Justice Information Authority">
-      <v-container py-0>
-        <v-layout row wrap justify-center>
-          <v-flex xs12 sm10 xl8>
-            <p>
-              Created in 1983, the Illinois Criminal Justice Information
-              Authority (ICJIA) is a state agency dedicated to improving the
-              administration of criminal justice.
-            </p>
-            <p>
-              ICJIA brings together key leaders from the justice system and the
-              public to identify critical issues facing the criminal justice
-              system in Illinois, and to propose and evaluate policies,
-              programs, and legislation that address those issues. The agency
-              also works to ensure the criminal justice system in Illinois is
-              efficient and effective. ICJIA’s specific powers and duties are
-              detailed in the Illinois Criminal Justice Information Act [
-              <a
-                href="http://www.ilga.gov/legislation/ilcs/ilcs3.asp?ActID=397&ChapterID=5"
-              >
-                20 ILCS 3930 et. seq.<v-icon>open_in_new</v-icon>
-              </a>
-              ].
-            </p>
-            <p>
-              The statutory responsibilities of ICJIA fall under the categories
-              of grants administration, research and analysis, policy and
-              planning, and information systems and technology.
-            </p>
-            <p>
-              For more information about ICJIA, please visit
-              <a href="http://www.icjia.state.il.us">
-                the official website<v-icon>open_in_new</v-icon>
-              </a>
-              .
-            </p>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </BaseSection>
+    <AboutSection :title="section2.title">
+      <p v-html="section2.p1"></p>
+      <p v-html="section2.p2"></p>
+      <p v-html="section2.p3"></p>
+      <p v-html="section2.p4"></p>
+    </AboutSection>
 
-    <BaseSection title="Illinois Open Data Portal">
-      <v-container py-0>
-        <v-layout row wrap justify-center>
-          <v-flex xs12 sm10 xl8>
-            <p>
-              The State of Illinois offers a state-wide open data portal, which
-              lets you find data across state, facts about your state, create
-              maps and graphs, and freely download the data for your own
-              analysis. Many of these datasets are updated daily, and some even
-              more often.
-            </p>
-            <p>
-              On March 10, 2014, the Illinois General Assembly unanimously
-              passed PA 98-0627, a bill introduced by the Governor's Office and
-              intended to increase transparency, accountability and savings in
-              government by establishing a new State Open Operating Standard.
-            </p>
-            <p>
-              For more information, please visit
-              <a href="https://data.illinois.gov/">
-                Illinois Open Data Portal<v-icon>open_in_new</v-icon>
-              </a>
-              .
-            </p>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </BaseSection>
+    <AboutSection :title="section3.title">
+      <p v-html="section3.p1"></p>
+      <p v-html="section3.p2"></p>
+      <p v-html="section3.p3"></p>
+    </AboutSection>
 
     <div class="spacer"></div>
   </div>
 </template>
 
 <script>
-import BaseSection from '@/components/BaseSection'
+import AboutSection from '@/components/AboutSection'
 import BaseViewTitle from '@/components/BaseViewTitle'
 
 export default {
   name: 'about',
   components: {
-    BaseSection,
+    AboutSection,
     BaseViewTitle
   },
   data() {
     return {
-      title: 'About'
+      title: 'About',
+      linkIconHtml:
+        '<i aria-hidden="true" class="v-icon material-icons theme--light">open_in_new</i>',
+      lorem30:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus commodi iste nihil assumenda quasi repudiandae nemo repellat, autem quis nulla pariatur sit, tempora dolorum facere vero sed illum consequuntur voluptas.'
+    }
+  },
+  computed: {
+    section1() {
+      return {
+        title: 'ICJIA Research Hub',
+        sub1: {
+          title: 'Open data, open research, open government',
+          p1: 'Explain the goals and purposes of the Research Hub here.',
+          p2: this.lorem30
+        },
+        sub2: {
+          title: 'Open source technology',
+          p1:
+            'The Research Hub is developed publicly on <a href="#"> GitHub' +
+            this.linkIconHtml +
+            '</a> with XXX license.',
+          p2:
+            'It is also powered by popular open source projects, including: <ul>' +
+            '<li><a href="https://vuejs.org/">Vue.js' +
+            this.linkIconHtml +
+            '</a> and <a href="https://vuetifyjs.com/">Vuetify' +
+            this.linkIconHtml +
+            '</a> for user interface;</li>' +
+            '<li><a href="https://vuepress.vuejs.org/">VuePress' +
+            this.linkIconHtml +
+            '</a> for documentation;</li>' +
+            '<li><a href="https://strapi.io/">Strapi' +
+            this.linkIconHtml +
+            '</a> for headless contenet management system;</li>' +
+            '<li><a href="https://www.docker.com/">Docker' +
+            this.linkIconHtml +
+            '</a> for for containerization;</li>' +
+            '<li>and many more.</li>'
+        },
+        sub3: {
+          title: 'Need your help',
+          p1: 'Explain how to provide feedback and contribution.',
+          p2: this.lorem30
+        }
+      }
+    },
+    section2() {
+      return {
+        title: 'Illinois Criminal Justice Information Authority',
+        p1:
+          'Created in 1983, the Illinois Criminal Justice Information Authority (ICJIA) is a state agency dedicated to improving the administration of criminal justice.',
+        p2:
+          `ICJIA brings together key leaders from the justice system and the public to identify critical issues facing the criminal justice system in Illinois, and to propose and evaluate policies, programs, and legislation that address those issues. The agency also works to ensure the criminal justice system in Illinois is efficient and effective. ICJIA’s specific powers and duties are detailed in the Illinois Criminal Justice Information Act [ <a href="http://www.ilga.gov/legislation/ilcs/ilcs3.asp?ActID=397&ChapterID=5">20 ILCS 3930 et. seq.` +
+          this.linkIconHtml +
+          '</a> ].',
+        p3:
+          'The statutory responsibilities of ICJIA fall under the categories of grants administration, research and analysis, policy and planning, and information systems and technology.',
+        p4:
+          'For more information about ICJIA, please visit <a href="http://www.icjia.state.il.us"> the official website' +
+          this.linkIconHtml +
+          '</a>.'
+      }
+    },
+    section3() {
+      return {
+        title: 'Illinois Open Data Portal',
+        p1:
+          'The State of Illinois offers a state-wide open data portal, which lets you find data across state, facts about your state, create maps and graphs, and freely download the data for your own analysis. Many of these datasets are updated daily, and some even more often.',
+        p2:
+          "On March 10, 2014, the Illinois General Assembly unanimously passed PA 98-0627, a bill introduced by the Governor's Office and intended to increase transparency, accountability and savings in government by establishing a new State Open Operating Standard.",
+        p3:
+          'For more information, please visit <a href="https://data.illinois.gov/"> Illinois Open Data Portal' +
+          this.linkIconHtml +
+          '</a>.'
+      }
     }
   }
 }
