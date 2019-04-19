f74ee57f4fe00fe182b5104f451a4a35aa7444bb
Bobae Kang <bkang.kr@gmail.com>
2019-04-19 13:24:50 -0500

    remove inject-initial-state

--- src/services/inject-initial-state.js
+++
@@ -1,5 +0,0 @@
-export default function injectInitialState(state) {
-  const script = document.createElement(`script`)
-  script.innerHTML = `window.__INITIAL_STATE__ = ${JSON.stringify(state)}`
-  document.head.appendChild(script)
-}
