70d06b791a907139a745275f42788844d89930f6
Bobae Kang <bkang.dev@gmail.com>
2019-05-17 13:29:11 -0500

    use vue-meta

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import NProgress from 'nprogress'
+import Meta from 'vue-meta'
 
 Vue.use(Router)
+Vue.use(Meta)
 
 const router = new Router({
   mode: 'history',
