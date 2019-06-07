16307f0e0cf19e9f3cf54547c4eda28c333ad8e4
Bobae Kang <bkang.dev@gmail.com>
2019-06-07 09:40:27 -0500

    add more routes to prerender

--- routes.js
+++ routes.js
@@ -10,15 +10,17 @@ main()
 async function main() {
   dotenv.config({ path: '.env.local' })
 
-  const routes = ['/', '/about']
+  const routes = ['/', '/about', '/apps', '/articles', '/authors', '/datasets']
   const client = axios.create({ baseURL: process.env.VUE_APP_API_BASE_URL })
 
   const appRoutes = await fetchAppRoutes(client)
   const articleRoutes = await fetchArticleRoutes(client)
+  const authorRoutes = await fetchAuthorRoutes(client)
   const datasetRoutes = await fetchDatasetRoutes(client)
 
   routes.push(...appRoutes)
   routes.push(...articleRoutes)
+  routes.push(...authorRoutes)
   routes.push(...datasetRoutes)
 
   try {
@@ -26,15 +28,15 @@ async function main() {
       if (error) throw error
     })
   } catch (e) {
-    console.log(e)
+    console.error(e)
   }
 }
 
-async function fetchAppRoutes(client) {
-  const res = await client
+async function baseFetchRoutes(client, type) {
+  return await client
     .post('graphql', {
       query: `{
-        apps {
+        ${type} {
           slug
         }
       }`
@@ -42,43 +44,31 @@ async function fetchAppRoutes(client) {
     .catch(err => {
       console.error(err)
     })
+}
 
+async function fetchAppRoutes(client) {
+  const res = await baseFetchRoutes(client, 'apps')
   return res.data.data.apps.map(el => {
     return `/apps/${el.slug}`
   })
 }
 
 async function fetchArticleRoutes(client) {
-  const res = await client
-    .post('graphql', {
-      query: `{
-        articles {
-          slug
-        }
-      }`
-    })
-    .catch(err => {
-      console.error(err)
-    })
-
+  const res = await baseFetchRoutes(client, 'articles')
   return res.data.data.articles.map(el => {
     return `/articles/${el.slug}`
   })
 }
 
-async function fetchDatasetRoutes(client) {
-  const res = await client
-    .post('graphql', {
-      query: `{
-        datasets {
-          slug
-        }
-      }`
-    })
-    .catch(err => {
-      console.error(err)
-    })
+async function fetchAuthorRoutes(client) {
+  const res = await baseFetchRoutes(client, 'authors')
+  return res.data.data.authors.map(el => {
+    return `/authors/${el.slug}`
+  })
+}
 
+async function fetchDatasetRoutes(client) {
+  const res = await baseFetchRoutes(client, 'datasets')
   return res.data.data.datasets.map(el => {
     return `/datasets/${el.slug}`
   })
